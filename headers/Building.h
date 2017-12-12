8ad1e7d9e500d72a58a92269ec2521c47059f486
Alexander Krapukhin <alekrap@gmail.com>
2017-12-12 02:39:39 +0100

    doxy

--- headers/Building.h
+++ headers/Building.h
@@ -1,17 +1,24 @@
-// Building.h
-// Building abstract base class.
-// git change
+/// \file
+/// Building.h
+/// Building abstract base class.
 #ifndef BUILDING_H
 #define BUILDING_H
 
 #include <string> // C++ standard string class
 
+/// Building abstract base class.
+///
+/// More detailed text.
 class Building {
 public:
+	/// constructor
 	Building();
+	/// destructor
 	virtual ~Building() = default; // compiler generates virtual destructor
+								   /// draw function
+	virtual void draw() const = 0; // pure virtual  
 
-	virtual void draw() const = 0; // pure virtual          
+								   /// text function
 	virtual std::string toString() const; // virtual    
 
 private:
