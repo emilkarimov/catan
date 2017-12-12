3953db0c2258fc55fe978ea2535f053b5b703b73
Alexander Krapukhin <alekrap@gmail.com>
2017-12-12 02:37:54 +0100

    board updated

--- headers/Board.h
+++ headers/Board.h
@@ -1,20 +1,32 @@
-// Board.h
-// Board class
+/// \file
+/// Board.h
+/// Board class
+///
 #ifndef BOARD_H
 #define BOARD_H
-//vs change
+
 #include <string> // C++ standard string class
 #include <array>
 #include "Tile.h"
 
+/// Board class
+///
+/// More detailed text.
 class Board {
 public:
-	Board(std::array<Tile, 37>* );
+	/// constructor
+	/// \param a array with pointers to 37 tiles
+	Board(std::array<Tile, 37>* a);
+	/// destructor
 	~Board() = default; // virtual destructor
 
+						/// draw function
 	void draw() const;
+
+	/// textual info
 	std::string toString() const;
 private:
+	/// pointers to tiles
 	std::array<Tile, 37>* tilesPtr;
 };
 
